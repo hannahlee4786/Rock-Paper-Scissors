3ee497f51f5de43c386c9c10de540008663a1fb8
hannahlee4786 <hlee4786@usc.edu>
2025-01-08 15:12:35 -0800

    add

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Rock-Paper-Scissors
 
-Interactive Rock Paper Scissors game coded using HTML, CSS, and Javascript.
+Interactive Rock Paper Scissors game coded using HTML, CSS, and Javascript. User can play against a computer or computer can auto play. Score is displayed and can be reset when necessary.
--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,13 +6,43 @@ let score = JSON.parse(localStorage.getItem('score')) || {
 
 updateScoreElement();
 
-/*if (!score) {
-  score = {
-    wins: 0,
-    losses: 0,
-    ties: 0
+let isAutoPlaying = false;
+let intervalId;
+
+function autoPlay() {
+  if(!isAutoPlaying) {
+    intervalId = setInterval(() => {
+      const playerMove = pickComputerMove();
+      playGame(playerMove);
+    }, 1000);
+    isAutoPlaying = true;
+  } 
+  else {
+    clearInterval(intervalId);
+    isAutoPlaying = false;
   }
-}*/
+  
+}
+
+document.querySelector('.js-rock-button').addEventListener('click', () => {
+  playGame('rock');
+});
+document.querySelector('.js-paper-button').addEventListener('click', () => {
+  playGame('paper');
+});
+document.querySelector('.js-scissors-button').addEventListener('click', () => {
+  playGame('scissors');
+});
+
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p') {
+    playGame('paper');
+  } else if (event.key === 's') {
+    playGame('scissors');
+  }
+})
 
 function playGame(playerMove) {
   let computerMove = pickComputerMove();
